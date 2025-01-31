d9e9fddfce47758edb0e4e163f76e1934dfd3496
Mohammed Berila <mberila@e3r9p2.1337.ma>
2025-01-31 12:34:23 +0100

    New Commit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mberila <mberila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 11:12:41 by mberila           #+#    #+#             */
-/*   Updated: 2025/01/31 11:12:42 by mberila          ###   ########.fr       */
+/*   Updated: 2025/01/31 12:14:34 by mberila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,45 +33,47 @@ void	print_stack(t_stack *stack)
 
 int main(void)
 {
-	t_stack	*stack_a;
-
-	stack_a = init_stack();
-
-    if (!stack_a)
+	t_stack *stack_a;
+    t_stack *stack_b;
+    
+    // Initialize stacks
+    stack_a = init_stack();
+    stack_b = init_stack();
+    if (!stack_a || !stack_b)
     {
         printf("Stack initialization failed\n");
         return (1);
     }
 
-   // Test Case 1: Empty stack
-    printf("\nTest Case 1: Empty stack\n");
-    sa(stack_a);
-    print_stack(stack_a);
-
-    // Test Case 2: Single element
-    printf("\nTest Case 2: Single element\n");
-    push_to_stack(stack_a, 42);
+    // Test sa
+    printf("\nTesting sa:\n");
+    push_to_stack(stack_a, 1);
+    push_to_stack(stack_a, 2);
+    push_to_stack(stack_a, 3);
     print_stack(stack_a);
     sa(stack_a);
     print_stack(stack_a);
 
-    // Test Case 3: Two elements
-    printf("\nTest Case 3: Two elements\n");
-    push_to_stack(stack_a, 21);
-    print_stack(stack_a);
-    sa(stack_a);
-    printf("After sa: ");
-    print_stack(stack_a);
+    // Test sb
+    printf("\nTesting sb:\n");
+    push_to_stack(stack_b, 10);
+    push_to_stack(stack_b, 20);
+    push_to_stack(stack_b, 30);
+    print_stack(stack_b);
+    sb(stack_b);
+    print_stack(stack_b);
 
-     // Test Case 4: Three elements
-    printf("\nTest Case 4: Three elements\n");
-    push_to_stack(stack_a, 33);
+    // Test ss
+    printf("\nTesting ss:\n");
     print_stack(stack_a);
-    sa(stack_a);
-    printf("After sa: ");
+    print_stack(stack_b);
+    ss(stack_a, stack_b);
+    printf("After ss:\n");
     print_stack(stack_a);
+    print_stack(stack_b);
 
-    // Free the stack (we'll implement proper free functions later)
+    // Free memory
     free(stack_a);
+    free(stack_b);
     return (0);
 }
--- src/operations.c
+++ src/operations.c
@@ -6,7 +6,7 @@
 /*   By: mberila <mberila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 11:12:52 by mberila           #+#    #+#             */
-/*   Updated: 2025/01/31 12:06:49 by mberila          ###   ########.fr       */
+/*   Updated: 2025/01/31 12:34:18 by mberila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,51 @@ void	sb(t_stack *stack_b)
 		return ;
 	first = stack_b->top;
 	second = first->next;
-
 	ft_swap(&(first->value), &(second->value));
 	write(1, "sb\n", 3);
 }
+
 void	ss(t_stack *stack_a, t_stack *stack_b)
 {
-	sa(stack_a);
-	sb(stack_b);
+	t_node	*first_a;
+	t_node	*second_a;
+	t_node	*first_b;
+	t_node	*second_b;
+
+	if (stack_a && stack_a->top && stack_a->top->next)
+	{
+		first_a = stack_a->top;
+		second_a = first_a->next;
+		ft_swap(&(first_a->value), &(second_a->value));
+	}
+	if (stack_b && stack_b->top && stack_b->top->next)
+	{
+		first_b = stack_b->top;
+		second_b = first_b->next;
+		ft_swap(&(first_b->value), &(second_b->value));
+	}
+	write(1, "ss\n", 3);
+}
+void	pa(t_stack *stack_a, t_stack *stack_b)
+{
+	t_node	*top_a;
+	t_node	*top_b;
+
+	top_a = stack_a->top;
+	top_b = stack_b->top;
+	push_to_stack(stack_a, top_b->value);
+	stack_b->size--;
+	stack_b->top->value = NULL;
+}
+
+void	pb(t_stack *stack_a, t_stack *stack_b)
+{
+	t_node	*top_a;
+	t_node	*top_b;
+
+	top_a = stack_a->top;
+	top_b = stack_b->top;
+	push_to_stack(stack_b, top_a->value);
+	stack_a->size--;
+	stack_a->top->value = NULL;
 }
