b060f03b0eac18d8b45829d8d93fcb1b5394786b
Mohammed Berila <mberila@e3r9p2.1337.ma>
2025-02-01 12:15:44 +0100

    New Commit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mberila <mberila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 11:12:41 by mberila           #+#    #+#             */
-/*   Updated: 2025/02/01 11:42:07 by mberila          ###   ########.fr       */
+/*   Updated: 2025/02/01 12:14:44 by mberila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,17 +51,17 @@ static t_stack	*initialize_stacks(t_stack **stack_a, t_stack **stack_b)
 	return (*stack_a);
 }
 
-void f(void)
-{
-	system ("leaks push_swap");
-}
+// void f(void)
+// {
+// 	system ("leaks push_swap");
+// }
 
 int main(int ac, char **av)
 {
 	t_stack *stack_a;
 	t_stack *stack_b;
 	
-	atexit(f);
+	// atexit(f);
 	if (!initialize_stacks(&stack_a, &stack_b))
 		clean_exit(stack_a, stack_b);
 	if (!process_args(ac, av, stack_a))
