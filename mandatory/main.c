6553bdfa6b12f1433ed6015cc433ba58d05b5713
Mohammed Berila <mberila@e3r9p2.1337.ma>
2025-02-18 11:34:06 +0100

    the problem found

--- mandatory/main.c
+++ mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: mberila <mberila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 11:12:41 by mberila           #+#    #+#             */
-/*   Updated: 2025/02/18 10:56:29 by mberila          ###   ########.fr       */
+/*   Updated: 2025/02/18 11:32:21 by mberila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,30 +39,14 @@ void	sort_stack(t_stack *stack_a, t_stack *stack_b)
 	int	size;
 
 	size = stack_a->size;
-	print_stack(stack_a, "Stack A before sorting");
-    print_stack(stack_b, "Stack B before sorting");
 	if (size == 2)
-	{
 		sa(stack_a);
-		print_stack(stack_a, "Stack A after sa");
-	}
 	else if (size == 3)
-	{
 		sort_three(stack_a);
-		print_stack(stack_a, "Stack A after sort_three");
-	}
 	else if (size <= 5)
-	{
 		sort_small(stack_a, stack_b);
-		print_stack(stack_a, "Stack A after sort_small");
-        print_stack(stack_b, "Stack B after sort_small");
-	}
 	else
-	{
 		sort_large(stack_a, stack_b);
-		print_stack(stack_a, "Stack A after sort_large");
-        print_stack(stack_b, "Stack B after sort_large");
-	}
 }
 
 // void	f(void)
@@ -82,15 +66,8 @@ int	main(int ac, char **av)
 		clean_exit(stack_a, stack_b);
 	if (!process_args(ac, av, stack_a))
 		clean_exit(stack_a, stack_b);
-		// Debug print after initialization
-    print_stack(stack_a, "Stack A after initialization");
-    print_stack(stack_b, "Stack B after initialization");
 	if (!is_sorted(stack_a))
-	{
 		sort_stack(stack_a, stack_b);
-		print_stack(stack_a, "Stack A after sorting");
-        print_stack(stack_b, "Stack B after sorting");
-	}
 	free_stack(stack_a);
 	free_stack(stack_b);
 	return (0);
